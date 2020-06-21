b0ecc008badb5655427d8e2838c5c86356af974c
Adam Pawliński <apawlinski@divante.pl>
2020-06-22 00:00:38 +0200

    vuetify add

---
+++ src/components/Main.vue
@@ -0,0 +1,57 @@
+<template>
+  
+    <v-system-bar> </v-system-bar>
+    <v-app-bar app>
+      <v-menu :open-on-click="true">
+         <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="primary"
+            dark
+            v-bind="attrs"
+            v-on="on"
+          >
+            Dropdown
+        </v-btn>
+        </template>
+      </v-menu>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify/lib'
+import vuetify from './plugins/vuetify';
+// import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify);
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+
+  // vuetify,
+  render: h => h(App)
 }).$mount('#app')
