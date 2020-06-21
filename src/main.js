66340e58d21e22a9b77919980200bd3bc6d5e553
Adam Pawliński <apawlinski@divante.pl>
2020-06-22 00:05:15 +0200

    config project with vuetify

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify/lib'
-import vuetify from './plugins/vuetify';
-// import vuetify from './plugins/vuetify'
+import router from './router'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify);
 
 new Vue({
   vuetify,
-
-  // vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
